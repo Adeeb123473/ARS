c50d5b934bbd679cd0d027ca2e032b9708f032d4
Mutayyab Imran <mutayyab786yo@gmail.com>
2023-06-22 23:29:47 +0500

    button design

---
+++ src/com/raven/swing/btn.java
@@ -0,0 +1,18 @@
+package com.raven.swing;
+
+import java.awt.*;
+import javax.swing.JButton;
+
+public class btn extends JButton {
+
+    public btn() {
+        setOpaque(true);
+        setBackground(null);
+        setBackground(Color.decode("#2C5364"));
+//        setForeground(Color.decode(""));
+setFont(new Font("Segoe UI",Font.BOLD,12));
+        setForeground(new Color(255,255,255));
+//        setBackground(white);
+    }
+    
+}
